e519d5f7e600fcf21c453aea8a9094ddc70fb1f0
salcines <salcines@outlook.es>
2025-05-03 22:28:59 +0200

    Array contains two numbers next to each other

--- Topics/Iterating over arrays/Check if an array contains two numbers/src/Main.java
+++ Topics/Iterating over arrays/Check if an array contains two numbers/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[scanner.nextInt()];
+        int numbersIndex = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        boolean straight = false;
+
+        for (int i = 0; i < numbersIndex; i++) {
+            if (first == numbers[i]  && second == numbers[i + 1]
+            || second == numbers[i] && first == numbers[i + 1]) {
+                straight = true;
+                break;
+            }
+        }
+
+        System.out.println(straight);
     }
 }
