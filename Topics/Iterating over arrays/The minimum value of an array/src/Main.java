388720100ed17c91f4a35f2de311f4284d4d95d2
salcines <salcines@outlook.es>
2025-05-04 03:46:30 +0200

    Iterating over Arrays - task 10. Minimum value of an array

--- Topics/Iterating over arrays/The minimum value of an array/src/Main.java
+++ Topics/Iterating over arrays/The minimum value of an array/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[scanner.nextInt()];
+
+        final int SIZE = numbers.length;
+
+        for (int i = 0; i < SIZE; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers[0]);
     }
 }
