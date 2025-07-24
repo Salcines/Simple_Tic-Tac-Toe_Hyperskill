8464cd5b7bf5bb352da8f1fff092ec8a94f945a3
salcines <salcines@outlook.es>
2025-07-24 23:30:48 +0200

    Processing strings. Task 03 - Encoding a given string

---
+++ Topics/Processing strings/Compression algorithm/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        String decompressed = input.nextLine();
+        StringBuilder output = new StringBuilder();
+        char characterToCount = decompressed.charAt(0);
+        int counter = 1;
+
+        for (int i = 1; i < decompressed.length(); i++) {
+            if (decompressed.charAt(i) == characterToCount) {
+                counter++;
+            } else {
+                output.append(characterToCount).append(counter);
+                characterToCount = decompressed.charAt(i);
+                counter = 1;
+            }
+        }
+
+        System.out.println(
+                output.append(
+                        characterToCount
+                ).append(counter));
+    }
+}
